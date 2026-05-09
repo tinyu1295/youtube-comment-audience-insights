35cf50f9f8352d347cdffe9703d179434acdeccd
tinyu1295-dev <tinyu@ecosphere.net>
2026-05-09 23:25:10 +0100

    complete version

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SMA-Sentiment-Intelligence-Plugin",
+    version="1.0.0",
+    author="TIN YU",
+    author_email="tinyu1295@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
